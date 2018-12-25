7cc1e6c6387da0fd60dc1af33016139fa06dc110
Unknown <khaufra.maggini@gmail.com>
2018-12-25 22:03:07 +0100

    s_micro - Part 02 Step 12 - Monitoring APIs

---
+++ spring_micorservices/Part02 - Restful Web Services with Spring Boot/Step12 - Monitoring APIs/restful-web-services/src/main/java/com/lordalucard90/springboottutorials/restfulwebservices/user/User.java
@@ -0,0 +1,59 @@
+package com.lordalucard90.springboottutorials.restfulwebservices.user;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+import java.util.Date;
+
+@ApiModel(description = "This is a social site user.")
+public class User {
+    private Integer id;
+
+    @Size(min = 2, message = "Name should have at least 2 characters")
+    @ApiModelProperty(notes = "Name should at least have 2 characters.")
+    private String name;
+
+    @Past
+    @ApiModelProperty(notes = "Birth date should be in the past.")
+    private Date birthDate;
+
+    protected User(){
+    }
+
+    public User(Integer id, String name, Date birthDate) {
+        this.id = id;
+        this.name = name;
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s, $s, $s", id, name, birthDate);
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+}
